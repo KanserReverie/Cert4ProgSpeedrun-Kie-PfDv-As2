95d0d36fe2b9504b72188a57d2cec9185a844704
Kieran <kieranc.inbox@gmail.com>
2021-09-10 12:39:19 +1000

    pushing

--- Assets/Cert 4 Speed Run/Scripts/UI/MainMenuUI.cs
+++ Assets/Cert 4 Speed Run/Scripts/UI/MainMenuUI.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuUI : MonoBehaviour
+namespace CertIVSpeedrun.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MainMenuUI : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
