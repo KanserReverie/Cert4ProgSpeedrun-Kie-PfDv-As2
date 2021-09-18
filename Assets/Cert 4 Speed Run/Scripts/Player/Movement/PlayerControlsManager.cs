c0b0beb1d7bb035aa068e67e120748cd63efda43
Kieran <kieranc.inbox@gmail.com>
2021-09-18 11:13:39 +1000

    Movement done weeks now need to be made

--- Assets/Cert 4 Speed Run/Scripts/Player/Movement/PlayerControlsManager.cs
+++ Assets/Cert 4 Speed Run/Scripts/Player/Movement/PlayerControlsManager.cs
@@ -29,8 +29,8 @@ namespace CertIVSpeedrun.Player
         
             public Rigidbody myRigidbody;
         
-            public float speed;
-            public float jumpForce;
+            public float speed = 2;
+            public float jumpForce = 4;
         
             public float fallMultiplier = 2.5f;
             public float lowJumpMultiplier = 2f;
@@ -43,7 +43,11 @@ namespace CertIVSpeedrun.Player
             public int defaultAdditionalJumps = 1;
             int additionalJumps;
             public bool isReady=false;
-        
+
+            [SerializeField] private bool leftButtonDown = false;
+            [SerializeField] private bool rightButtonDown = false;
+            [SerializeField] private bool jumpButtonDown= false;
+            
         
         
             void Start()
@@ -61,24 +65,75 @@ namespace CertIVSpeedrun.Player
                     CheckIfGrounded();
                 }
             }
-        
-        
-            void Move() {
-                float x = Input.GetAxisRaw("Horizontal");
-        
-                float moveBy = x * speed;
-        
-                myRigidbody.velocity = new Vector2(moveBy, myRigidbody.velocity.y);
+
+
+            // This will be for the move left button.
+            public void MoveLeftButton()
+            {
+                leftButtonDown = true;
             }
-        
-            void Jump() {
-                if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || additionalJumps > 0)) {
-                    myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
-                    additionalJumps--;
+            public void MoveLeftButtonUp()
+            {
+                leftButtonDown = false;
+            }
+            
+            // This will be for the move right button.
+            public void MoveRightButton()
+            {
+                rightButtonDown = true;
+            }
+            // This will be for the move right button.
+            public void MoveRightButtonUp()
+            {
+                rightButtonDown = false;
+            }
+            // This will be for the jump button.
+            public void JumpButton()
+            {
+                jumpButtonDown = true;
+            } 
+            // This will be for the jump button.
+            public void JumpButtonUp()
+            {
+                jumpButtonDown = false;
+            } 
+
+            public void Move()
+            {
+                float x = Input.GetAxis("Horizontal");
+                if(leftButtonDown || rightButtonDown)
+                {
+                    if(leftButtonDown && rightButtonDown)
+                    {
+                        x = 0;
+                    }
+                    else if (leftButtonDown)
+                    {
+                        x = -1;
+                    }
+                    else if (rightButtonDown)
+                    {
+                        x = 1;
+                    }
+                }
+                float moveBy = x * speed;
+                myRigidbody.velocity = new Vector3(moveBy, myRigidbody.velocity.y,0);
+            }
+
+            public void Jump()
+            {
+                if(Input.GetButtonDown("Jump") || jumpButtonDown)
+                {
+                    if(isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || additionalJumps > 0)
+                    {
+                        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+                        additionalJumps--;
+                    }
                 }
             }
-        
-            void BetterJump() {
+
+            public void BetterJump() 
+            {
                 if (myRigidbody.velocity.y < 0)
                 {
                     Vector2 HoriVerti = Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
